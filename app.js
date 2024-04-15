f5a4612ba726d46e82278b66fb69b6adcd94c47a
Aloice Ogana <aloiceadriot@gmail.com>
2024-04-15 17:04:47 +0300

    const including list of roman numerals

--- app.js
+++ app.js
@@ -2,3 +2,22 @@ let input = document.getElementById("input");
 let button = document.getElementById("submit");
 let errorMessage = document.getElementById("error");
 let output = document.getElementById("output");
+
+
+const romanObject = {
+  M: 1000,
+  CM: 900,
+  D: 500,
+  CD: 400,
+  C: 100,
+  XC: 90,
+  L: 50,
+  XL: 40,
+  XXX: 30,
+  XX: 20,
+  X: 10,
+  IX: 9,
+  V: 5,
+  IV: 4,
+  I: 1,
+};
