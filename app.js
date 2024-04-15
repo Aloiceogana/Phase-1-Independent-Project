e6834be824363ef320000b1be74c9d4b7383aefe
Aloice Ogana <aloiceadriot@gmail.com>
2024-04-15 17:03:07 +0300

    js let statements an docs

---
+++ app.js
@@ -0,0 +1,4 @@
+let input = document.getElementById("input");
+let button = document.getElementById("submit");
+let errorMessage = document.getElementById("error");
+let output = document.getElementById("output");
